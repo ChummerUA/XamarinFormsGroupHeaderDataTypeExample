73e1bd36eb72ba10bae8186b3997a5f2fef6571e
Herman Yurchenko <chummer1008@gmail.com>
2021-07-10 15:17:48 +0300

    Add project files.

---
+++ XamarinFormsGroupHeaderDataTypeExample/XamarinFormsGroupHeaderDataTypeExample/GroupObservableCollection.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.ObjectModel;
+
+namespace XamarinFormsGroupHeaderDataTypeExample
+{
+    public class GroupObservableCollection<T> : ObservableCollection<T>, IGroupHeader
+    {
+        public string Title { get; set; }
+    }
+}
---
+++ XamarinFormsGroupHeaderDataTypeExample/XamarinFormsGroupHeaderDataTypeExample/IGroupHeader.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinFormsGroupHeaderDataTypeExample
+{
+    public interface IGroupHeader
+    {
+        string Title { get; }
+    }
+}
